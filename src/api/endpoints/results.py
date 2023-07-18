81bb3a4abf2a96337741df829b9820a641569143
shahafalo <shahafalo@gmail.com>
2023-07-18 22:35:05 +0200

    fix protest vote result

--- src/api/endpoints/results.py
+++ src/api/endpoints/results.py
@@ -8,7 +8,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from src.schemas.result import Result, Winner
 from src.crud import vote_crud
 from src.db.utils import get_db
-from src.results_manager.manager import ResultsManager
+from src.results_manager.manager import ResultsManager, PROTEST_VOTE_KEY
 
 router = APIRouter()
 manager = ResultsManager()
@@ -20,7 +20,8 @@ async def get_results(date: Union[datetime.date, None] = None, db: Session = Dep
         date = datetime.date.today()
     votes = vote_crud.get_votes_history(start_date=date, end_date=date + datetime.timedelta(days=1), db=db)
     results = manager.calculate_results(votes)
-    return [Result(restaurant_id=res[0] if res[0] else None, score=res[1]) for res in results.items()]
+    return [Result(restaurant_id=res[0] if res[0] != PROTEST_VOTE_KEY else None,
+                   score=res[1]) for res in results.items()]
 
 
 @router.get("/history/{start_date}", response_model=list[Result])
@@ -36,7 +37,8 @@ async def get_results_history(start_date: str, end_date: Union[str, None] = None
                                         restaurant_ids=restaurant_ids,
                                         db=db)
     results = manager.calculate_results(votes)
-    return [Result(restaurant_id=res[0], score=res[1]) for res in results.items()]
+    return [Result(restaurant_id=res[0] if res[0] != PROTEST_VOTE_KEY else None,
+                   score=res[1]) for res in results.items()]
 
 
 @router.get("/winner/", response_model=Winner)
